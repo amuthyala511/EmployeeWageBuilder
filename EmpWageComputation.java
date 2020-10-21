48c21447faa27573e24409818bfc5fa29a59e62f
Aishwarya <amuthyala511@gmail.com>
2020-10-21 21:19:01 +0530

    added part time and calculated employee daily wage

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -2,20 +2,28 @@ public class EmpWageComputation
 {
 	public static void main(String[] args)
 	{
-		int empHrs=8,empWage=0;
-		int empWageperHr=20;
 		System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION ON MASTER BRANCH");
-		int IS_PRESENT=1;
-		double empAttendance=Math.floor(Math.random()*10)%2;
-		if(empAttendance == IS_PRESENT)
+		final int IS_PART_TIME=1;
+		final int IS_FULL_TIME=2;
+		final int EMP_WAGE_PER_HR=20;
+		int empHrs=0, empWage=0;
+		double empCheck=Math.floor(Math.random()*10)%3;
+		if(empCheck == IS_PART_TIME)
 		{
-			System.out.println("Employee is Present");
-			empWage=empHrs*empWageperHr;
-			System.out.println("Employee Daily Wage is: "+empWage);
+			System.out.println("Employee worked part time");
+			empHrs=4;
+		}
+		else if(empCheck == IS_FULL_TIME)
+		{
+			System.out.println("Employee worked full time");
+			empHrs=8;
 		}
 		else
 		{
 			System.out.println("Employee is Absent");
+			empHrs=0;
 		}
+		empWage=empHrs*EMP_WAGE_PER_HR;
+		System.out.println("Employee Daily Wage is: "+empWage);
 	}
 }
