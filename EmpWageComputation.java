cfe84ca0d99ba92e50d5b7555f80f26be1985bc1
Aishwarya <amuthyala511@gmail.com>
2020-10-21 20:53:37 +0530

    calculated employee daily wage for full day working hours

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -2,12 +2,16 @@ public class EmpWageComputation
 {
 	public static void main(String[] args)
 	{
+		int empHrs=8,empWage=0;
+		int empWageperHr=20;
 		System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION ON MASTER BRANCH");
 		int IS_PRESENT=1;
 		double empAttendance=Math.floor(Math.random()*10)%2;
 		if(empAttendance == IS_PRESENT)
 		{
 			System.out.println("Employee is Present");
+			empWage=empHrs*empWageperHr;
+			System.out.println("Employee Daily Wage is: "+empWage);
 		}
 		else
 		{
