347e32e6e8d85d226358d200b1cd1486f809c77d
Aishwarya <amuthyala511@gmail.com>
2020-10-21 19:05:29 +0530

    added code to check employee's attendance

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -3,5 +3,15 @@ public class EmpWageComputation
 	public static void main(String[] args)
 	{
 		System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION ON MASTER BRANCH");
+		int IS_PRESENT=1;
+		double empAttendance=Math.floor(Math.random()*10)%2;
+		if(empAttendance == IS_PRESENT)
+		{
+			System.out.println("Employee is Present");
+		}
+		else
+		{
+			System.out.println("Employee is Absent");
+		}
 	}
 }
